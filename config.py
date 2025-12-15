cf3136e5c5e64887ba58253b6ef1bf650947cbd1
Umid5045 <ibodov_u@mail.ru>
2025-12-15 14:53:31 +0500

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Bot tokeni
-BOT_TOKEN = os.getenv("7941352882:AAGOWK2bmXVX4b4od_bNoQtTR5vrBfeJmKs")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 # Admin ID lari (bir nechta bo'lishi mumkin)
 ADMIN_IDS = [
-    int(admin_id) for admin_id in os.getenv("ADMIN_IDS", "7970818314").split(",") if admin_id
+    int(admin_id) for admin_id in os.getenv("ADMIN_IDS", "").split(",") if admin_id
 ]
